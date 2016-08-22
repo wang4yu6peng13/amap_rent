4e0a2b57712c4aa7cb550980c11e02063e50c030
wang4yu6peng13 <wang4yu6peng13@qq.com>
2016-08-22 16:03:26 +0800

    crawl infomation, write amap page

--- crawl.py
+++ crawl.py
@@ -10,3 +10,47 @@ url = "http://bj.58.com/pinpaigongyu/pn/{page}/?minprice=0_2500"
 #已完成的页数序号，初时为0
 page = 0
 
+csv_file = open("rent.csv","wb")
+csv_writer = csv.writer(csv_file, delimiter=',')
+
+while True:
+    page += 1
+    print "fetch: ", url.format(page=page)
+    response = requests.get(url.format(page=page))
+    html = BeautifulSoup(response.text)
+    house_list = html.select(".list > li")
+
+    # 循环在读不到新的房源时结束
+    if not house_list:
+        break
+
+    for house in house_list:
+        house_title = house.select("h2")[0].string.encode("utf8")
+        house_url = urljoin(url, house.select("a")[0]["href"])
+        house_info_list = house_title.split()
+
+        # 如果第二列是公寓名则取第一列作为地址
+        if "公寓" in house_info_list[1] or "青年社区" in house_info_list[1]:
+            house_location = house_info_list[0]
+        else:
+            house_location = house_info_list[1]
+
+        house_money = house.select(".money")[0].select("b")[0].string.encode("utf8")
+        csv_writer.writerow([house_title, house_location, house_money, house_url])
+
+csv_file.close()
+
+'''
+/usr/local/lib/python2.7/dist-packages/bs4/__init__.py:181: UserWarning: No parser was explicitly specified, so I'm using the best available HTML parser for this system ("html.parser"). This usually isn't a problem, but if you run this code on another system, or in a different virtual environment, it may use a different parser and behave differently.
+
+The code that caused this warning is on line 20 of the file crawl.py. To get rid of this warning, change code that looks like this:
+
+ BeautifulSoup([your markup])
+
+to this:
+
+ BeautifulSoup([your markup], "html.parser")
+
+  markup_type=markup_type))
+
+'''
